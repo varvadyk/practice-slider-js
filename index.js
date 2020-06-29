a924a6395b954a98b368e5fac09039d8d1eb6907
Diana <diana_ivanivna@kr.net>
2020-06-29 22:56:49 +0300

    add slider js

---
+++ index.js
@@ -0,0 +1,5 @@
+var slider1=sliderFactory.getNewSlider();
+var slider2=sliderFactory.getNewSlider();
+
+slider1.start('#slider1');
+slider2.start('#slider2');
---
+++ slyder2.js
@@ -0,0 +1,2 @@
+
+ var slider2=getNewSlider();
